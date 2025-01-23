d73f915cffc36f21d9646e3c78bbb50afa17b37f
Gabrielle <143741502+gabifpp0@users.noreply.github.com>
2025-01-23 01:48:56 +0000

    Adicionando o template e css

--- projcadastroCV/cadastroCV/views.py
+++ projcadastroCV/cadastroCV/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Formulario
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse("OI!")
+    formulario = Formulario.objects.all()
+    context = {'formulario': formulario}
+    return render(request, 'cadastro.html', context)
